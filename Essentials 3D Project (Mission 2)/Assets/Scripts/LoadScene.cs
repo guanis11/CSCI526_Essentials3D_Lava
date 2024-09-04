ba60ced8593958c182d96e0299641f452d737bcc
Isabel Guan <iguan@usc.edu>
2024-09-04 13:18:12 -0700

    Essentials 3D Project Upload

---
+++ Essentials 3D Project (Mission 2)/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public void Load3DEssentials()
+    {
+        SceneManager.LoadScene("EssentialsScene");
+    }
+
+    public void LoadLavaChallenge()
+    {
+        SceneManager.LoadScene("FLoorIsLavaChallengeScene");
+    }
+}
